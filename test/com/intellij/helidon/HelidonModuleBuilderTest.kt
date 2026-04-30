7becc1db5d303f25a2150692bd3e7bda6567c093
Daniel Kec <daniel.kec@oracle.com>
2026-04-30 14:07:17 +0200

    Fix #5 refresh Helidon starter templates

--- test/com/intellij/helidon/HelidonModuleBuilderTest.kt
+++ test/com/intellij/helidon/HelidonModuleBuilderTest.kt
@@ -4,8 +4,12 @@ package com.intellij.helidon
 import com.intellij.helidon.newproject.HelidonModuleBuilder
 import com.intellij.ide.starters.local.StarterModuleBuilder.Companion.setupTestModule
 import com.intellij.ide.starters.shared.*
+import com.intellij.pom.java.LanguageLevel
 import com.intellij.testFramework.fixtures.LightJavaCodeInsightFixtureTestCase
 import com.intellij.testFramework.fixtures.LightJavaCodeInsightFixtureTestCase4
+import com.intellij.util.lang.JavaVersion
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertTrue
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.junit.runners.Parameterized
@@ -13,7 +17,7 @@ import org.junit.runners.Parameterized.Parameter
 import org.junit.runners.Parameterized.Parameters
 
 @RunWith(Parameterized::class)
-class HelidonModuleBuilderTest : LightJavaCodeInsightFixtureTestCase4(LightJavaCodeInsightFixtureTestCase.JAVA_11) {
+class HelidonModuleBuilderTest : LightJavaCodeInsightFixtureTestCase4(LightJavaCodeInsightFixtureTestCase.JAVA_21) {
   @Parameter(0)
   lateinit var generateLanguage: String
 
@@ -45,32 +49,76 @@ class HelidonModuleBuilderTest : LightJavaCodeInsightFixtureTestCase4(LightJavaC
 
   @Test
   fun generateProject() {
-    HelidonModuleBuilder().setupTestModule(fixture.module) {
+    val moduleBuilder = HelidonModuleBuilder()
+    assertEquals(LanguageLevel.JDK_21.toJavaVersion(), getMinJavaVersion(moduleBuilder))
+
+    moduleBuilder.setupTestModule(fixture.module) {
       isCreatingNewProject = true
       language = LANGUAGES[generateLanguage]!!
       projectType = BUILD_SYSTEMS[generateBuildSystem]!!
     }
 
-    expectFile("src/main/resources/META-INF/beans.xml", BEANS_XML)
-    expectFile("src/main/resources/META-INF/microprofile-config.properties", MICROPROFILE_CONFIG)
+    expectFileContains(
+      "src/main/resources/META-INF/beans.xml",
+      "https://jakarta.ee/xml/ns/jakartaee/beans_4_0.xsd",
+      "version=\"4.0\"",
+      "bean-discovery-mode=\"annotated\""
+    )
+    expectFileContains("src/main/resources/META-INF/microprofile-config.properties", "server.port=8080")
+    expectBuildMetadata()
   }
 
-  private fun expectFile(path: String, content: String) {
+  private fun expectBuildMetadata() {
+    if (generateBuildSystem == "maven") {
+      expectFileContains(
+        "pom.xml",
+        "<version>4.4.1</version>",
+        "<maven.compiler.target>21</maven.compiler.target>",
+        "<maven.compiler.source>21</maven.compiler.source>",
+        "<maven.compiler.release>21</maven.compiler.release>",
+        "<groupId>io.smallrye</groupId>",
+        "<artifactId>jandex</artifactId>",
+        "<groupId>io.helidon.microprofile.testing</groupId>",
+        "<artifactId>helidon-microprofile-testing-junit5</artifactId>"
+      )
+      expectFileContains(
+        ".mvn/wrapper/maven-wrapper.properties",
+        "apache-maven/3.9.15/apache-maven-3.9.15-bin.zip",
+        "maven-wrapper/3.3.4/maven-wrapper-3.3.4.jar"
+      )
+      if (generateLanguage == "kotlin") {
+        expectFileContains("pom.xml", "<kotlin.version>2.3.21</kotlin.version>", "<jvmTarget>21</jvmTarget>")
+      }
+    }
+    else {
+      expectFileContains(
+        "build.gradle",
+        "id 'org.kordamp.gradle.jandex' version '2.3.0'",
+        "sourceCompatibility = JavaVersion.VERSION_21",
+        "targetCompatibility = JavaVersion.VERSION_21",
+        "io.helidon:helidon-dependencies:4.4.1",
+        "runtimeOnly('io.smallrye:jandex')",
+        "testImplementation('io.helidon.microprofile.testing:helidon-microprofile-testing-junit5')"
+      )
+      expectFileContains("gradle/wrapper/gradle-wrapper.properties", "gradle-9.2.0-bin.zip")
+      if (generateLanguage == "kotlin") {
+        expectFileContains("build.gradle", "id 'org.jetbrains.kotlin.jvm' version '2.3.21'", "jvmTarget = '21'")
+      }
+    }
+  }
+
+  private fun expectFileContains(path: String, vararg expectedSnippets: String) {
     fixture.configureFromTempProjectFile(path)
-    fixture.checkResult(content)
+    val actual = fixture.file.text
+    expectedSnippets.forEach {
+      assertTrue("Expected $path to contain:\n$it\n\nActual content:\n$actual", actual.contains(it))
+    }
   }
 
-  private val BEANS_XML: String = """
-    <?xml version="1.0" encoding="UTF-8"?>
-    <beans xmlns="https://jakarta.ee/xml/ns/jakartaee"
-           xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-           xsi:schemaLocation="https://jakarta.ee/xml/ns/jakartaee https://jakarta.ee/xml/ns/jakartaee/beans_3_0.xsd"
-           bean-discovery-mode="annotated">
-    
-    </beans>
-  """.trimIndent()
+  private fun getMinJavaVersion(moduleBuilder: HelidonModuleBuilder): JavaVersion {
+    val method = HelidonModuleBuilder::class.java.getDeclaredMethod("getMinJavaVersion")
+    method.isAccessible = true
+    return method.invoke(moduleBuilder) as JavaVersion
+  }
 
-  private val MICROPROFILE_CONFIG: String = """
-    server.port=8080
-  """.trimIndent()
 }
